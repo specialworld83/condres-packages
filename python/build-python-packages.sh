09e41ef13d46e9daaf32dca0fc8cd606212dfb83
Codelinsoft <specialworld83@gmail.com>
2018-09-19 19:16:03 +0200

    [uppkg] New stable release add. Fix python packages and support python 3.7

---
+++ python/build-python-packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+pwd=`pwd`
+
+if [ "`sudo cat /etc/sudoers | grep pacman`" == "" ] ; then
+   echo "please add '`whoami` ALL=NOPASSWD: /usr/bin/pacman' to your /etc/sudoers file"
+   exit 1
+fi
+
+echo 'cleaning environment'
+rm -R ${pwd}/*/{src,pkg} -f
+echo 'building extramodules'
+cd ${pwd}/*python-dpkt && makepkg -sf --noconfirm -d
+cd ${pwd}/*python-grpcio && makepkg -sf --noconfirm -d
+cd ${pwd}/*python-grpcio-tools && makepkg -sf --noconfirm -d
+cd ${pwd}/*python-iptables && makepkg -df --noconfirm -d
+cd ${pwd}/*python-jade-application-kit && makepkg -sf --noconfirm -d
+cd ${pwd}/*python-marisa-trie && makepkg -sf -d --noconfirm -d
+cd ${pwd}/*python-netfilterqueue && makepkg -d --noconfirm -d
+cd ${pwd}/*python-powerline-git && makepkg -d --noconfirm -d
+cd ${pwd}/*python-pychromecast && makepkg -sf --noconfirm -d
+cd ${pwd}/*python-pydbus && makepkg -sf --noconfirm -d
+cd ${pwd}/*python2-mutagen && makepkg -sf -d --noconfirm -d
+echo 'create repo'
+mkdir -p ${pwd}/repo-`uname -m`
+mv ${pwd}/*/*`uname -m`.pkg* ${pwd}/repo-`uname -m`
+ls ${pwd}/repo-`uname -m`
+echo 'building python packages'
