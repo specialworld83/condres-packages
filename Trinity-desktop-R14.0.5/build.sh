06cba37672ec92beb14e21162a8107f7ffbbe1d0
Condres OS Team <info@codelinsoft.it>
2019-01-16 16:56:25 +0100

    add packages to build trinity desktop

--- Trinity-desktop-R14.0.5/build.sh
+++ Trinity-desktop-R14.0.5/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 dir=$PWD
-for pkg in tqt3 tqtinterface arts dbus-tqt dbus-1-tqt tqca-tls libart-lgpl avahi-tqt tdelibs tdebase; do
+for pkg in tqt3 tqtinterface arts dbus-tqt dbus-1-tqt tqca-tls libart-lgpl avahi-tqt tdelibs tdebase dbus-1-tqt dbus-tqt ksplash libart-lgpl libcaldav libcarddav libkdcraw libkexiv2 lipstick qtcurve tdeaccessibility tdeaddons tdeadmin tdeartwork tdebindings tdeedu tdegames tdegraphics tdemultimedia tdenetwork tdepim tdesdk tdetoys tdeutils tdevelop tdewebdev tqca-tls  ; do
   cd "$dir"/tde-"$pkg"
   makepkg -Lsci
 done
