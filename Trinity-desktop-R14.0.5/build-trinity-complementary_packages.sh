f5060216154d5f502e14ee5088a642d082ed19b4
Condres OS Team <info@codelinsoft.it>
2019-01-17 09:20:08 +0100

    [uppkg] add build trinity complementary packages and fix build packages

---
+++ Trinity-desktop-R14.0.5/build-trinity-complementary_packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+pwd=`pwd`
+
+if [ "`sudo cat /etc/sudoers | grep pacman`" == "" ] ; then
+   echo "please add '`whoami` ALL=NOPASSWD: /usr/bin/pacman' to your /etc/sudoers file"
+   exit 1
+fi
+
+echo 'cleaning environment'
+rm -R ${pwd}/*/{src,pkg} -f
+echo 'building extramodules'
+cd ${pwd}/*tde-dbus-1-tqt && makepkg -sf --noconfirm
+cd ${pwd}/*tde-dbus-tqt && makepkg -sf --noconfirm
+cd ${pwd}/*tde-ksplash && makepkg -sf --noconfirm
+cd ${pwd}/*tde-libart-lgpl && makepkg -df --noconfirm
+cd ${pwd}/*tde-libcaldav && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tde-libcarddav && makepkg -sf -d --noconfirm
+cd ${pwd}/*tde-libkdcraw && makepkg -d --noconfirm
+cd ${pwd}/*tde-libkexiv2 && makepkg -d --noconfirm
+cd ${pwd}/*tde-lipstick && makepkg -sf --noconfirm
+cd ${pwd}/*tde-qtcurve && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdeaccessibility && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdeaddons && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdeadmin && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdeartwork && makepkg -df --noconfirm
+cd ${pwd}/*tde-tdebindings && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdeedu && makepkg -sf -d --noconfirm
+cd ${pwd}/*tde-tdegames && makepkg -d --noconfirm
+cd ${pwd}/*tde-tdegraphics && makepkg -d --noconfirm
+cd ${pwd}/*tde-tdemultimedia && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdenetwork && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdepim && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdesdk && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdetoys && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdeutils && makepkg -df --noconfirm
+cd ${pwd}/*tde-tdevelop && makepkg -sf --noconfirm
+cd ${pwd}/*tde-tdewebdev && makepkg -sf -d --noconfirm
+cd ${pwd}/*tde-tqca-tls && makepkg -d --noconfirm
+echo 'copy packages'
+mv ${pwd}/*/*`uname -m`.pkg* /srv/ftp/userftp/mirror/
+echo 'building extra packages done'
+rm -Rf ${pwd}/*/{src}
--- Trinity-desktop-R14.0.5/build.sh
+++ Trinity-desktop-R14.0.5/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 dir=$PWD
-for pkg in tqt3 tqtinterface arts dbus-tqt dbus-1-tqt tqca-tls libart-lgpl avahi-tqt tdelibs tdebase dbus-1-tqt dbus-tqt ksplash libart-lgpl libcaldav libcarddav libkdcraw libkexiv2 lipstick qtcurve tdeaccessibility tdeaddons tdeadmin tdeartwork tdebindings tdeedu tdegames tdegraphics tdemultimedia tdenetwork tdepim tdesdk tdetoys tdeutils tdevelop tdewebdev tqca-tls  ; do
+for pkg in tqt3 tqtinterface arts dbus-tqt dbus-1-tqt tqca-tls libart-lgpl avahi-tqt tdelibs tdebase   ; do
   cd "$dir"/tde-"$pkg"
   makepkg -Lsci
 done
+cp -rv "$dir"/tde-"$pkg"/*.pkg.tar.xz /srv/ftp/userftp/mirror/
 
 pwd=`pwd`
 rm -Rf "$dir"/tde-"$pkg"/{src,pkg}
