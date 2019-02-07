ecd2513e10b0b22905f982e60deb6486cf04d90a
Condres OS Team <info@codelinsoft.it>
2019-02-07 11:03:29 +0100

    [uppkg] Remove check_mk and nagios-nrpe-plugin

--- nagios/build-nagios-complete.sh
+++ nagios/build-nagios-complete.sh
@@ -12,9 +12,7 @@ rm -R ${pwd}/*/{src,pkg} -f
 echo 'building extramodules'
 cd ${pwd}/*nagios && makepkg -sf --noconfirm
 cd ${pwd}/*mod_python2 && makepkg -df --noconfirm
-cd ${pwd}/*check_mk && makepkg -sf --noconfirm
 cd ${pwd}/*check_md_raid && makepkg -sf -d --noconfirm
-cd ${pwd}/*nagios-nrpe-plugin && makepkg -d --noconfirm
 cd ${pwd}/*nagiosql && makepkg -d --noconfirm
 mv ${pwd}/*/*`uname -m`.pkg* /srv/ftp/userftp/mirror/
 rm -R ${pwd}/*/{src,pkg} -f
