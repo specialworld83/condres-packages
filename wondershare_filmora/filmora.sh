534b0dd0f0e9de47aa54aa5604e62594ab61dcca
specialworld83 <specialworld83@gmail.com>
2019-02-26 19:45:53 +0100

    [uppkg] update kernel version new and fix filmora wine setup

--- wondershare_filmora/filmora.sh
+++ wondershare_filmora/filmora.sh
@@ -2,9 +2,9 @@
   WINEPREFIX=$HOME/.wine/ winetricks vcrun2015
   WINEPREFIX=$HOME/.wine/ winetricks directx9
   wget https://github.com/doitsujin/dxvk/releases/download/v0.96/dxvk-0.96.tar.gz -O $HOME/.wine/dxvk-0.96.tar.gz
-  WINEPREFIX=$HOME/.wine/ tar xvzf dxvk-0.96.tar.gz
-  WINEPREFIX=$HOME/.wine/ cd dxvk-0.96
-  WINEPREFIX=$HOME/.wine/ winetricks --force setup_dxvk.verb
+  $HOME/.wine/ tar xvzf dxvk-0.96.tar.gz
+  $HOME/.wine/ cd dxvk-0.96
+  $HOME/.wine/ winetricks --force setup_dxvk.verb
   cd ..
   echo "-- run winecfg and change version to windowsxp to windows7 --"
   echo "-- add opencl.dll and msvcr120.dll to library --"
